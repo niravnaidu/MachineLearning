56e9db00ac8f02c5cbedb031b9365994882b0d18
Nirav Naidu <niravnaid@gmail.com>
2025-03-23 15:37:51 -0500

    first

---
+++ Notes.R
@@ -0,0 +1,15 @@
+#How to find square root 2
+sqrt(5000)
+
+is.numeric("A")
+
+as.logical(0)
+
+#as. changes it to something (not sure)
+
+# table(x) is a frequency table
+# hist() is a histogram
+# plot() is a barplot
+
+# assoc(y~x)
+# use data() to load data from datasets that are already in R
